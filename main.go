4f61ce91de06ec1815c5acd3b8593628a3edca76
Charles Dixon <chvckd@gmail.com>
2021-04-08 19:52:32 +0100

    Add support for gocbv1 and gocbcorev7

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"os"
+	"runtime/pprof"
 	"sync"
 	"sync/atomic"
 	"time"
 
 	"github.com/couchbase/go-couchbase"
+	gocbv1 "github.com/couchbase/gocb"
 	"github.com/couchbase/gocb/v2"
+	gocbcorev7 "github.com/couchbase/gocbcore"
 	"github.com/couchbase/gocbcore/v9"
 	"github.com/couchbase/goutils/logging"
 )
@@ -75,7 +79,7 @@ func selectOneKey(docCount int) string {
 }
 
 func main() {
-	addr := flag.String("host", "172.23.111.134", "The address to connect to")
+	addr := flag.String("host", "172.23.111.135", "The address to connect to")
 	username := flag.String("username", "Administrator", "The username to use")
 	password := flag.String("password", "password", "The password to use")
 	bucket := flag.String("bucket", "default", "The bucket to use")
@@ -90,14 +94,12 @@ func main() {
 
 	flag.Parse()
 
-	/*
-		f, err := os.Create("cpu.prof")
-		if err != nil {
-			log.Fatal(err)
-		}
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
-	*/
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
 
 	if flag.NArg() < 1 {
 		panic("must specify a test to run (gocb, gocbcore, gocouchbase)")
@@ -115,9 +117,15 @@ func main() {
 	case "gocb":
 		log.Printf("Running `gocb` benchmark...")
 		benchGocb(*duration, *addr, *username, *password, *docCount, *numConcurrentOps)
+	case "gocbv1":
+		log.Printf("Running `gocbv1` benchmark...")
+		benchGocbv1(*duration, *addr, *username, *password, *docCount, *numConcurrentOps)
 	case "gocbcore":
 		log.Printf("Running `gocbcore` benchmark...")
 		benchGocbcore(*duration, *addr, *username, *password, *docCount, *connPoolSize, *maxQueueSize, *numConcurrentOps)
+	case "gocbcorev7":
+		log.Printf("Running `gocbcorev7` benchmark...")
+		benchGocbcorev7(*duration, *addr, *username, *password, *docCount, *connPoolSize, *maxQueueSize, *numConcurrentOps)
 	case "gocouch":
 		log.Printf("Running `gocouch` benchmark...")
 		benchGocouch(*duration, *addr, *username, *password, *docCount, *numConcurrentOps)
@@ -209,6 +217,98 @@ func benchGocb(duration time.Duration, addr, username, password string, docCount
 	cluster.Close(nil)
 }
 
+func benchGocbv1(duration time.Duration, addr, username, password string, docCount int, numConcurrentOps int) {
+	connStr := fmt.Sprintf("couchbase://%s", addr)
+	cluster, err := gocbv1.Connect(connStr)
+	if err != nil {
+		panic(err)
+	}
+	cluster.Authenticate(
+		gocbv1.PasswordAuthenticator{
+			Username: username,
+			Password: password,
+		})
+
+	bucket, err := cluster.OpenBucket("default", "")
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait until connections are ready by performing a bunch of random operations
+	numWarmupOps := 1024
+	warmWaitCh := make(chan struct{}, numWarmupOps)
+	for i := 0; i < numWarmupOps; i++ {
+		go func() {
+			var b []byte
+			bucket.Get(selectOneKey(docCount), &b)
+			warmWaitCh <- struct{}{}
+		}()
+	}
+	for i := 0; i < numWarmupOps; i++ {
+		<-warmWaitCh
+	}
+
+	var benchRunning uint32 = 1
+	var attlSuccess, attlError, attlOps, attlTime uint64
+	var wg sync.WaitGroup
+
+	var sendOneGet func()
+	sendOneGet = func() {
+		stime := time.Now()
+
+		var b []byte
+		_, err := bucket.Get(selectOneKey(docCount), &b)
+
+		etime := time.Now()
+		dtimeMs := etime.Sub(stime).Milliseconds()
+
+		atomic.AddUint64(&attlOps, 1)
+		atomic.AddUint64(&attlTime, uint64(dtimeMs))
+		if err != nil {
+			log.Printf("failed to send get %v", err)
+			atomic.AddUint64(&attlError, 1)
+		} else {
+			atomic.AddUint64(&attlSuccess, 1)
+		}
+
+		if atomic.LoadUint32(&benchRunning) == 1 {
+			sendOneGet()
+		} else {
+			wg.Done()
+		}
+	}
+
+	stime := time.Now()
+
+	endWaitCh := time.After(duration)
+
+	for i := 0; i < numConcurrentOps; i++ {
+		wg.Add(1)
+		go sendOneGet()
+	}
+
+	<-endWaitCh
+	atomic.StoreUint32(&benchRunning, 0)
+	wg.Wait()
+
+	etime := time.Now()
+	dtimeMs := etime.Sub(stime).Milliseconds()
+
+	ttlSuccess := atomic.LoadUint64(&attlSuccess)
+	ttlError := atomic.LoadUint64(&attlError)
+	ttlOps := atomic.LoadUint64(&attlOps)
+	ttlTime := atomic.LoadUint64(&attlTime)
+
+	log.Printf("gocbv1:")
+	log.Printf("	Ran for %dms", dtimeMs)
+	log.Printf("	Read %d docs", ttlSuccess)
+	log.Printf("	Had %d errors", ttlError)
+	log.Printf("	Average docs/s of %.2f", float64(ttlSuccess)/float64(dtimeMs)*1000)
+	log.Printf("	Average latency of %f milliseconds", float64(ttlTime)/float64(ttlOps))
+
+	cluster.Close()
+}
+
 func benchGocbcore(duration time.Duration, addr, username, password string, docCount int, kvPoolSize, maxQueueSize, numConcurrentOps int) {
 	connStr := fmt.Sprintf("couchbase://%s", addr)
 	config := gocbcore.AgentConfig{}
@@ -234,7 +334,9 @@ func benchGocbcore(duration time.Duration, addr, username, password string, docC
 	warmWaitCh := make(chan struct{}, 1)
 	_, err = agent.WaitUntilReady(
 		time.Now().Add(5*time.Second),
-		gocbcore.WaitUntilReadyOptions{},
+		gocbcore.WaitUntilReadyOptions{
+			RetryStrategy: gocbcore.NewBestEffortRetryStrategy(nil),
+		},
 		func(result *gocbcore.WaitUntilReadyResult, err error) {
 			if err != nil {
 				panic(err)
@@ -257,6 +359,107 @@ func benchGocbcore(duration time.Duration, addr, username, password string, docC
 		_, err = agent.Get(gocbcore.GetOptions{
 			Key: []byte(selectOneKey(docCount)),
 		}, func(result *gocbcore.GetResult, err error) {
+			etime := time.Now()
+			dtimeMs := etime.Sub(stime).Milliseconds()
+
+			atomic.AddUint64(&attlOps, 1)
+			atomic.AddUint64(&attlTime, uint64(dtimeMs))
+			if err != nil {
+				log.Printf("failed to send get %v", err)
+				atomic.AddUint64(&attlError, 1)
+			} else {
+				atomic.AddUint64(&attlSuccess, 1)
+			}
+
+			if atomic.LoadUint32(&benchRunning) == 1 {
+				sendOneGet()
+			} else {
+				wg.Done()
+			}
+		})
+		if err != nil {
+			log.Printf("failed to send get %v", err)
+			atomic.AddUint64(&attlError, 1)
+		}
+	}
+
+	stime := time.Now()
+
+	endWaitCh := time.After(duration)
+
+	for i := 0; i < numConcurrentOps; i++ {
+		wg.Add(1)
+		sendOneGet()
+	}
+
+	<-endWaitCh
+	atomic.StoreUint32(&benchRunning, 0)
+	wg.Wait()
+
+	etime := time.Now()
+	dtimeMs := etime.Sub(stime).Milliseconds()
+
+	ttlSuccess := atomic.LoadUint64(&attlSuccess)
+	ttlError := atomic.LoadUint64(&attlError)
+	ttlOps := atomic.LoadUint64(&attlOps)
+	ttlTime := atomic.LoadUint64(&attlTime)
+
+	log.Printf("gocbcore:")
+	log.Printf("	Ran for %dms", dtimeMs)
+	log.Printf("	Read %d docs", ttlSuccess)
+	log.Printf("	Had %d errors", ttlError)
+	log.Printf("	Average docs/s of %.2f", float64(ttlSuccess)/float64(dtimeMs)*1000)
+	log.Printf("	Average latency of %f milliseconds", float64(ttlTime)/float64(ttlOps))
+
+	agent.Close()
+}
+
+func benchGocbcorev7(duration time.Duration, addr, username, password string, docCount int, kvPoolSize, maxQueueSize, numConcurrentOps int) {
+	connStr := fmt.Sprintf("couchbase://%s", addr)
+	config := gocbcorev7.AgentConfig{}
+	err := config.FromConnStr(connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	config.KvPoolSize = kvPoolSize
+	config.MaxQueueSize = maxQueueSize
+	config.Auth = &gocbcorev7.PasswordAuthProvider{
+		Username: username,
+		Password: password,
+	}
+	config.BucketName = "default"
+
+	agent, err := gocbcorev7.CreateAgent(&config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait until connections are ready by performing a bunch of random operations
+	numWarmupOps := 1024
+	warmWaitCh := make(chan struct{}, numWarmupOps)
+	for i := 0; i < numWarmupOps; i++ {
+		agent.GetEx(gocbcorev7.GetOptions{
+			Key: []byte(selectOneKey(docCount)),
+		}, func(result *gocbcorev7.GetResult, err error) {
+			warmWaitCh <- struct{}{}
+		})
+	}
+	for i := 0; i < numWarmupOps; i++ {
+		<-warmWaitCh
+	}
+
+	// Do the benchmark
+	var benchRunning uint32 = 1
+	var attlSuccess, attlError, attlOps, attlTime uint64
+	var wg sync.WaitGroup
+
+	var sendOneGet func()
+	sendOneGet = func() {
+		stime := time.Now()
+		_, err = agent.GetEx(gocbcorev7.GetOptions{
+			Key: []byte(selectOneKey(docCount)),
+		}, func(result *gocbcorev7.GetResult, err error) {
 			etime := time.Now()
 			dtimeMs := etime.Sub(stime).Milliseconds()
 
